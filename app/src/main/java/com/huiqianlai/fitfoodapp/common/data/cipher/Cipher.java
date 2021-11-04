5006ae9920031dc541dad0532cfd9b068cdd9bb0
laihuiqian <laihuiqian951@126.com>
2021-11-04 22:30:55 +0100

    add util classes

---
+++ app/src/main/java/com/huiqianlai/fitfoodapp/common/data/cipher/Cipher.java
@@ -0,0 +1,9 @@
+package com.huiqianlai.fitfoodapp.common.data.cipher;
+
+/**
+ * @author MaTianyu
+ * @date 14-7-31
+ */
+public abstract class Cipher implements Encrypt,Decrypt{
+
+}
---
+++ app/src/main/java/com/huiqianlai/fitfoodapp/common/data/cipher/Encrypt.java
@@ -0,0 +1,9 @@
+package com.huiqianlai.fitfoodapp.common.data.cipher;
+
+/**
+ * @author MaTianyu
+ * @date 14-7-31
+ */
+public interface Encrypt {
+    public byte[] encrypt(byte[] res);
+}
