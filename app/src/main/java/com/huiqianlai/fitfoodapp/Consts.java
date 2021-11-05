1ba31f1abe8e752c2780a999597d6145bbfd2ba7
laihuiqian <laihuiqian951@126.com>
2021-11-05 09:59:20 +0100

    network develop

---
+++ app/src/main/java/com/huiqianlai/fitfoodapp/Consts.java
@@ -0,0 +1,6 @@
+package com.huiqianlai.fitfoodapp;
+
+public class Consts {
+    // todo need to revise
+    public static String BASE_URL = "http://192.168.31.242:8888/okHttpServer/";
+}
--- app/src/main/java/com/huiqianlai/fitfoodapp/RegisterActivity.java
+++ app/src/main/java/com/huiqianlai/fitfoodapp/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.huiqianlai.fitfoodapp;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -10,8 +11,21 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.huiqianlai.fitfoodapp.okhttp.OkHttpUtils;
+import com.huiqianlai.fitfoodapp.okhttp.callback.StringCallback;
+
+import java.util.HashMap;
+
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.Request;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
     private EditText mNameEditText;
+    private EditText mEmailEditText;
     private EditText mPasswordEditText;
     private EditText mConfirmPasswordEditText;
     private EditText mHeight;
@@ -30,6 +44,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void setUI() {
         mNameEditText = findViewById(R.id.set_your_user_name);
+        mEmailEditText = findViewById(R.id.set_your_email);
         mPasswordEditText = findViewById(R.id.set_your_password);
         mConfirmPasswordEditText = findViewById(R.id.comfirm_your_password);
         mHeight = findViewById(R.id.edit_body_height);
@@ -49,6 +64,11 @@ public class RegisterActivity extends AppCompatActivity {
 
                 if (TextUtils.isEmpty(mNameEditText.getText())) {
                     Toast.makeText(view.getContext(), "Please fill in your name!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(mEmailEditText.getText())) {
+                    Toast.makeText(view.getContext(), "Please fill in your email!", Toast.LENGTH_LONG).show();
                     return;
                 }
 
@@ -78,5 +98,72 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void doRegister() {
         //request http
+        postRegister();
+    }
+
+    private void postRegister() {
+        String url = Consts.BASE_URL + "register";
+
+        HashMap<String, Object> params = new HashMap<String, Object>();
+        params.put("name", mNameEditText.getText());
+        params.put("email", mEmailEditText.getText());
+        if (mMaleCheckBox.isChecked()) {
+            params.put("gender", "MALE");
+        } else {
+            params.put("gender", "FEMALE");
+        }
+        params.put("weight", Integer.parseInt(mWeight.getText().toString()));
+        params.put("height", Integer.parseInt(mHeight.getText().toString()));
+        params.put("password", mPasswordEditText.getText());
+        params.put("password_confirmation", mConfirmPasswordEditText.getText());
+
+        OkHttpUtils
+                .postString()
+                .url(url)
+                .mediaType(MediaType.parse("application/json; charset=utf-8"))
+                .content(params.toString())
+                .build()
+                .execute(new MyStringCallback());
+    }
+
+
+    public class MyStringCallback extends StringCallback {
+        @Override
+        public void onBefore(Request request, int id) {
+            setTitle("loading...");
+        }
+
+        @Override
+        public void onAfter(int id) {
+            setTitle("Fitfood-okHttp");
+        }
+
+        @Override
+        public void onError(Call call, Exception e, int id) {
+            e.printStackTrace();
+            Log.e("laihuiqian", "onError:" + e.getMessage());
+        }
+
+        @Override
+        public void onResponse(String response, int id) {
+            Log.e(TAG, "onResponse：complete");
+            Log.d("laihuiqian", "onResponse:" + response);
+
+            switch (id) {
+                case 100:
+                    Toast.makeText(RegisterActivity.this, "http", Toast.LENGTH_SHORT).show();
+                    break;
+                case 101:
+                    Toast.makeText(RegisterActivity.this, "https", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+
+        @Override
+        public void inProgress(float progress, long total, int id) {
+            Log.e(TAG, "inProgress:" + progress);
+            Log.d("laihuiqian", "inProgress:" + progress);
+//            mProgressBar.setProgress((int) (100 * progress));
+        }
     }
 }
