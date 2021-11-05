25772d5d8166dec6278d84100981725a7231ac1e
laihuiqian <laihuiqian951@126.com>
2021-11-05 09:43:21 +0100

    add gender

--- app/src/main/java/com/huiqianlai/fitfoodapp/RegisterActivity.java
+++ app/src/main/java/com/huiqianlai/fitfoodapp/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,6 +17,8 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mHeight;
     private EditText mWeight;
     private Button mRegister;
+    private CheckBox mMaleCheckBox;
+    private CheckBox mFemaleCheckBox;
 
 
     @Override
@@ -31,6 +34,9 @@ public class RegisterActivity extends AppCompatActivity {
         mConfirmPasswordEditText = findViewById(R.id.comfirm_your_password);
         mHeight = findViewById(R.id.edit_body_height);
         mWeight = findViewById(R.id.edit_body_weight);
+        mMaleCheckBox = findViewById(R.id.checkbox_male);
+        mFemaleCheckBox = findViewById(R.id.checkbox_female);
+
         mRegister = findViewById(R.id.register);
 
         mRegister.setOnClickListener(new View.OnClickListener() {
