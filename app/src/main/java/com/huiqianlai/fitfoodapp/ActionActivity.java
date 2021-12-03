a3145c26be38e65a8e032621202b5ac379d3bc1c
Money <laihuiqian951@126.com>
2021-12-03 02:31:55 +0100

    提交被删除的代码

--- app/src/main/java/com/huiqianlai/fitfoodapp/ActionActivity.java
+++ app/src/main/java/com/huiqianlai/fitfoodapp/ActionActivity.java
@@ -256,32 +256,6 @@ public class ActionActivity extends FragmentActivity {
     }
 
 
-    private List<Fruit> fruitList = new ArrayList<Fruit>();
-
-    private void initFruits() {
-        for (int i = 0; i < 2; i++) {
-            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic);
-            fruitList.add(apple);
-            Fruit banana = new Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic);
-            fruitList.add(banana);
-            Fruit orange = new Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic);
-            fruitList.add(orange);
-            Fruit watermelon = new Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic);
-            fruitList.add(watermelon);
-            Fruit pear = new Fruit(getRandomLengthName("Pear"), R.drawable.pear_pic);
-            fruitList.add(pear);
-            Fruit grape = new Fruit(getRandomLengthName("Grape"), R.drawable.grape_pic);
-            fruitList.add(grape);
-            Fruit pineapple = new Fruit(getRandomLengthName("Pineapple"), R.drawable.pineapple_pic);
-            fruitList.add(pineapple);
-            Fruit strawberry = new Fruit(getRandomLengthName("Strawberry"), R.drawable.strawberry_pic);
-            fruitList.add(strawberry);
-            Fruit cherry = new Fruit(getRandomLengthName("Cherry"), R.drawable.cherry_pic);
-            fruitList.add(cherry);
-            Fruit mango = new Fruit(getRandomLengthName("Mango"), R.drawable.mango_pic);
-            fruitList.add(mango);
-        }
-    }
 
     private String getRandomLengthName(String name) {
         Random random = new Random();
